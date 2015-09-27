e04740c387b83a2cc580c94a6a7958a51c4c2471
John Powell <john.powell@britishmicro.com>
2015-09-27 12:52:30 +0100

    First CheckIn, Solution structure

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+
+        // Problem
+        // Write a program that prints the numbers from 1 to 100. 
+        // But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". 
+        // For numbers which are multiples of both three and five print "FizzBuzz".
+
+        static void Main(string[] args)
+        {
+
+            Enumerable.Range(1, 100).ToList().ForEach(i =>
+                Console.WriteLine(
+                    (i % 3 == 0 && i % 5 == 0) ? "FizzBuzz" :
+                    (i % 5 == 0) ? "Buzz" :
+                    (i % 3 == 0) ? "Fizz" :
+                    i.ToString()
+                )
+            );
+
+            //for (int i = 1; i <= 100; i++)
+            //{
+            //    string fizz = (i % 3 == 0 ? "Fizz" : "");
+            //    string buzz = (i % 5 == 0 ? "Buzz" : "");
+            //    string number = i.ToString();
+            //    if (fizz.Length > 0 || buzz.Length > 0)
+            //    {
+            //        number = string.Empty;
+            //    }
+            //    Console.WriteLine("{0}{1}{2}", fizz, buzz, number);
+            //}
+
+            Console.ReadLine();
+        }
+    }
+}
